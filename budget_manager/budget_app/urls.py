ec913bc381f4221ef8d6bd7cf812668b07e2532a
Moslem Miah <moslemgis@gmail.com>
2026-06-03 00:21:40 +0530

    add  and config delete button to budget table

--- budget_manager/budget_app/urls.py
+++ budget_manager/budget_app/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.view_budget, name="budget_page"),
     path('add/', views.add_budget, name="add_budget"),
+    path('delete/<int:pk>/', views.delete_budget, name="delete_budget"),
 ]
--- budget_manager/budget_app/views.py
+++ budget_manager/budget_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Budget
 from .forms import BudgetForm
 
@@ -21,3 +21,14 @@ def add_budget(request):
 def view_budget(request):
     budgets = Budget.objects.all()
     return render(request, "budget_app/budget.html", {"budgets": budgets})
+
+# delete budget item
+# def delete_budget(request, pk):
+#     if request.method == "POST":
+#         budget = Budget.objects.get(id=pk)
+#         budget.delete()
+#         return redirect('budget_page')
+def delete_budget(request, pk):
+    budget = get_object_or_404(Budget, pk=pk)
+    budget.delete()
+    return redirect('budget_page')
