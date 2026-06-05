a4cc255ba1c1ac2d422c37bf58b991647df394b3
Moslem Miah <moslemgis@gmail.com>
2026-06-05 07:06:17 +0530

    sett fix

--- budget_manager/budget_manager/settings.py
+++ budget_manager/budget_manager/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATABASE_URL= dj_database_url.config(default='postgresql://postgres:6675@localhost/budget')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-s*42d&7)w!dg7-qdq1oa2xt(!o_@ccc_wn^rx=ep2ir_4leruw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
